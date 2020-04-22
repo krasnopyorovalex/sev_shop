eca8f7bff61a9403d6812fbe254ffb287f9e530a
Sanek <djShtaket88@mail.ru>
2020-04-22 15:40:04 +0300

    add header Cookie to InitStep

--- app/Domain/Commerce/HttpSteps/InitStep.php
+++ app/Domain/Commerce/HttpSteps/InitStep.php
@@ -6,13 +6,13 @@ namespace Domain\Commerce\HttpSteps;
 
 final class InitStep extends Step
 {
-    private const MAX_FILE_SIZE = 8388608;
+    private const MAX_FILE_SIZE = 2000*1024; //2Mb
 
     public function handle(): void
     {
         //$this->verifyCookie();
 
-        \Log::info((string)$this->request->headers);
+        \Log::info((string)$this->request->header('Cookie'));
 
         $this->status = sprintf('%s' . PHP_EOL . '%s',
             'zip=yes',
