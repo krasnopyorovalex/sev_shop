d564b0289a4d44e0136a1dad97b681255b25bb51
Sanek <djShtaket88@mail.ru>
2020-04-24 17:13:58 +0300

    cahnge path in  put FileStep

--- app/Domain/Commerce/HttpSteps/FileStep.php
+++ app/Domain/Commerce/HttpSteps/FileStep.php
@@ -15,7 +15,7 @@ final class FileStep extends Step
         }
 
         $filename = $this->request->get('filename');
-        $path = public_path("1c_catalog/{$filename}");
+        $path = storage_path("public/1c_catalog/{$filename}");
 
         $content = $this->request->getContent();
 
