d228d0575d8e9d695782320f117ef4cf7cf6dab2
krasnopyorovalex <djShtaket88@mail.ru>
2020-05-27 17:28:46 +0300

    cahnge validator for cart order request for date

--- app/Domain/Cart/Requests/CartOrderRequest.php
+++ app/Domain/Cart/Requests/CartOrderRequest.php
@@ -18,7 +18,7 @@ class CartOrderRequest extends Request
             'name' => 'bail|string|required|min:3|max:128|regex:/[А-Яа-яЁё]/u',
             'phone' => 'required|string|max:18',
             'address' => 'required|string|max:255',
-            'date' => 'required|date_format:"d.m.Y"|after_or_equal:today',
+            'date' => 'required|date_format:"d.m.Y"|after:today',
             'time' => 'required|string',
             'comment' => 'string|nullable'
         ];
